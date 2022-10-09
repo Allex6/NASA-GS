956234f01a0680826a4046a9370df02efd42d3c2
Alex Souza <alex.webdevelloper@gmail.com>
2022-10-09 14:20:24 -0300

    feat: adds unauthorized error handler

--- lib/middlewares/errorHandler.ts
+++ lib/middlewares/errorHandler.ts
@@ -4,10 +4,11 @@ function errorHandler(error: { type: string, message: string | number }, req: Re
 
     console.log('Error handler called', error);
 
-    if(error.type === 'bad_request') res.status(400).send(error.message);
-    if(error.type === 'not_found') res.status(404).send(error.message);
-    if(error.type === 'conflict') res.status(409).send(error.message);
-    if(error.type === 'unprocessable_entity') res.status(422).send(error.message);
+    if(error.type === 'bad_request') return res.status(400).send(error.message);
+    if(error.type === 'not_found') return res.status(404).send(error.message);
+    if(error.type === 'conflict') return res.status(409).send(error.message);
+    if(error.type === 'unprocessable_entity') return res.status(422).send(error.message);
+    if(error.type === 'unauthorized') return res.status(422).send(error.message);
 
     return res.sendStatus(500);
 
